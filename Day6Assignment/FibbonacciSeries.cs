7f4fca3067a99f1f82de17c09c280167dca65319
Wpaswan <dablumcapaswan@gmail.com>
2021-11-24 18:49:12 -0800

    Uc1 Fibbo Series added

---
+++ Day6Assignment/FibbonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment
+{
+    internal class FibbonacciSeries
+    {
+        public void Fibbo()
+        {
+            int firstnumber = 0, secondnumber = 1, result = 0;
+            Console.WriteLine("Enter the number terms");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Fibbo Series upto "+n+" terms is:\t");
+            Console.Write(firstnumber);
+            for (int i = 2; i<= n; i++)  // main processing starts from here
+            {
+                result = firstnumber + secondnumber;
+                firstnumber = secondnumber;
+                secondnumber = result;
+                Console.Write("\t"+result);
+            }
+
+
+        }
+    }
+}
---
+++ Day6Assignment/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day6Assignment;
+Console.WriteLine("Hello, World!");
+FibbonacciSeries fs = new FibbonacciSeries();
+fs.Fibbo();
