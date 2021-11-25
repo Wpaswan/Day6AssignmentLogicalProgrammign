2506c2f050c001ac3c656aaa91aa0e2c463738c8
Wpaswan <dablumcapaswan@gmail.com>
2021-11-24 20:43:15 -0800

    Uc3 to check prime is added

---
+++ Day6Assignment/PrimeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment
+{
+    internal class PrimeNumber
+    {
+        public void prime()
+        {
+            Console.WriteLine("Enter any number to check whether it prime or not!!!");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if(n%i == 0)
+                    count++;
+
+            }
+            if (count == 2)
+                Console.WriteLine(n+" is a prime number.");
+            else
+                Console.WriteLine(n+" is not a prime number.");
+        }
+    }
+}
--- Day6Assignment/Program.cs
+++ Day6Assignment/Program.cs
@@ -3,7 +3,7 @@ using Day6Assignment;
 Console.WriteLine("\t((((((Please select any one))))))");
 Console.WriteLine("\t((((((1.Fibbo Series       ))))))");
 Console.WriteLine("\t((((((2.Perfect Number     ))))))");
-Console.WriteLine("\t((((((Please select any one))))))");
+Console.WriteLine("\t((((((3.Prime Number       ))))))");
 int ch=Convert.ToInt32(Console.ReadLine());
 switch(ch){
     case 1:
@@ -13,5 +13,9 @@ fs.Fibbo();
             case 2:
 PerfectNumber pf=new PerfectNumber();
 pf.Perfect();
+        break;
+        case 3:
+        PrimeNumber p=new PrimeNumber();
+        p.prime();
         break;
 }
