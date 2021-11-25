8872fdfc8b97446be44f0d53443844c6e4994bbf
Wpaswan <dablumcapaswan@gmail.com>
2021-11-24 20:30:46 -0800

    Uc2 Perfect number is added

--- Day6Assignment/Program.cs
+++ Day6Assignment/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Day6Assignment;
-Console.WriteLine("Hello, World!");
+Console.WriteLine("\t((((((Please select any one))))))");
+Console.WriteLine("\t((((((1.Fibbo Series       ))))))");
+Console.WriteLine("\t((((((2.Perfect Number     ))))))");
+Console.WriteLine("\t((((((Please select any one))))))");
+int ch=Convert.ToInt32(Console.ReadLine());
+switch(ch){
+    case 1:
 FibbonacciSeries fs = new FibbonacciSeries();
 fs.Fibbo();
+        break; ;
+            case 2:
+PerfectNumber pf=new PerfectNumber();
+pf.Perfect();
+        break;
+}
