3f6bd2bf4ead0ce881f7057a9a0d0b369c719e2e
Wpaswan <dablumcapaswan@gmail.com>
2021-11-25 21:25:35 -0800

    Uc7 VendingMachin added

--- Day6Assignment/Program.cs
+++ Day6Assignment/Program.cs
@@ -7,9 +7,12 @@ Console.WriteLine("\t((((((3.Prime Number       ))))))");
 Console.WriteLine("\t((((((4.Reverse Number     ))))))");
 Console.WriteLine("\t((((((5.Coupon Number      ))))))");
 Console.WriteLine("\t((((((6.Stop Watch         ))))))");
+Console.WriteLine("\t((((((7.Vending Machine    ))))))");
+Console.WriteLine("\t((((((8.Bhawani            ))))))");
 
-int ch =Convert.ToInt32(Console.ReadLine());
-switch(ch){
+
+int Aloo =Convert.ToInt32(Console.ReadLine());
+switch(Aloo){
     case 1:
 FibbonacciSeries fs = new FibbonacciSeries();
 fs.Fibbo();
@@ -35,6 +38,16 @@ pf.Perfect();
     case 6:
         StopWatch s=new StopWatch();
         s.RunProgram();
+        break;
+    case 7:
+        Vendingmachine vm=new Vendingmachine();
+        Console.WriteLine("Enter the amount:");
+        int amount=Convert.ToInt32(Console.ReadLine());
+        vm.countCurrency(amount);
+        break;
+    case 8:
+        
+        
         break;
 
 }
---
+++ Day6Assignment/Vendingmachine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment
+{
+    public class Vendingmachine
+    {
+        // function to count and
+        // print currency notes
+        public void countCurrency(int amount)
+        {
+            int[] notes = new int[]{ 2000, 500, 200, 100,
+                     50, 20, 10, 5, 1 };
+            int[] noteCounter=new int[9];
+
+            // count notes using Greedy approach
+            for (int i = 0; i < 9; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount - noteCounter[i] * notes[i];
+                    Console.WriteLine(amount);
+                }
+            }
+            
+            // Print notes
+            Console.WriteLine("currency count ->");
+            int[] notes1 = new int[]{ 2000, 500, 200, 100,
+                     50, 20, 10, 5, 1 };
+            int largestnote;
+            for (int i = 0; i < 9; i++)
+            {
+                if (noteCounter[i] != 0)
+                {
+                    Console.WriteLine(notes[i]+" : "+noteCounter[i]);
+                       
+                }
+               
+            }
+            
+            
+        }
+    }
+}
