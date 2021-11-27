1e4c957a172c941225ef575a7864a32d21bb6153
Wpaswan <dablumcapaswan@gmail.com>
2021-11-27 08:12:11 -0800

    Uc9 Temprature Conversion added

---
+++ Day6Assignment/TemperatureConversion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment
+{
+    internal class TemperatureConversion
+    { public static  void Temperature_Coversion()
+        {
+            Console.WriteLine("\t(((((((   Please Select  any        )))))))");
+            Console.WriteLine("\t(((((((   1.Celcius to Fahrenheit   )))))))");
+            Console.WriteLine("\t(((((((   1.Fahrenheit to Celcius   )))))))");
+            int ch=Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    Console.WriteLine("\tEnter the temperature in celcius");
+                    double c=Convert.ToDouble(Console.ReadLine());
+                    double f = (c*9/5)+32;
+                    Console.WriteLine("Temperature in Fahrenheit="+f);
+                    break;
+                    case 2:
+                    Console.WriteLine("\tEnter the temperature in Fahrenheit");
+                    double fr = Convert.ToDouble(Console.ReadLine());
+                    double cel = (fr-32)*9/5;
+                    Console.WriteLine("Temperature in Celcius="+cel);
+                    break;
+            }
+
+
+        }
+    }
+}
